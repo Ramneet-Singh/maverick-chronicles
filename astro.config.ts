dd51db6d4a9b40d14029575c6ca8a0eed4c5b4db
Ramneet Singh <ramneet2001@gmail.com>
2025-08-14 19:47:15 +0530

    Bug Fix

--- astro.config.ts
+++ astro.config.ts
@@ -14,6 +14,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  base: "maverick-chronicles",
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
