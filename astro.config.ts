31019f902558f9961fd216be2e5c7fb58ae98f8d
Ramneet Singh <63390014+Ramneet-Singh@users.noreply.github.com>
2025-08-14 20:54:13 +0530

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -14,7 +14,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  base: "maverick-chronicles",
+  base: "/maverick-chronicles",
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
